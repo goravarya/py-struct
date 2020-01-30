89927eb152a5364e8522b1fdd21be7335a4b9513
cristian-rincon <cristian.o.rincon.b@gmail.com>
2020-01-29 20:27:07 -0500

    new revision

--- app/templates/prepare_environment.sh
+++ app/templates/prepare_environment.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pipenv install --dev pytest pylint coverage pytest-cov sphinx sphinx-rtd-theme && pipenv shell && git init
+pipenv install --dev pytest pylint coverage tox pytest-cov sphinx sphinx-rtd-theme && pipenv shell && git init
